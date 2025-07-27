651fff44822974a862a089bb4c3b1de3ca56b59d
Thomas Ralls <tralls.pnw@gmail.com>
2025-07-26 17:24:26 -0700

    Basic email config

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -2,6 +2,7 @@ import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
@@ -49,5 +50,17 @@ export default buildConfig({
   }),
   sharp,
   plugins: [],
+  email: nodemailerAdapter({
+    defaultFromAddress: process.env.EMAIL_FROM_ADDRESS ?? '',
+    defaultFromName: process.env.EMAIL_FROM_NAME ?? '',
+    transportOptions: {
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT) ?? 587,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    },
+  }),
   blocks: allBlocks,
 })
